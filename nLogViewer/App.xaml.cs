d386ad3127d9c815e51e9dd76079a9c4800218bc
DadonovAD <DadonovAD@tvv.transneft.ru>
2022-11-17 09:37:21 +0300

    Мелкие правки

--- nLogViewer/App.xaml.cs
+++ nLogViewer/App.xaml.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
 using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using System.Windows;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
