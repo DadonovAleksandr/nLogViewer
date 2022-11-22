9dbe5cfdc6f8586416a09261f6bb8be8d60ce64d
DadonovAD <DadonovAD@tvv.transneft.ru>
2022-11-22 11:19:11 +0300

    Фильтрация без МВВМ

--- nLogViewer/Views/LogViewerView.xaml.cs
+++ nLogViewer/Views/LogViewerView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using nLogViewer.Model;
+using nLogViewer.ViewModels;
 
 namespace nLogViewer.Views;
 
@@ -7,5 +11,25 @@ public partial class LogViewerView : UserControl
     public LogViewerView()
     {
         InitializeComponent();
+    }
+
+    private void LogEntryCollection_OnFilter(object sender, FilterEventArgs e)
+    {
+        if(!(e.Item is LogEntryView entry)) return;
+            
+        var filterText = LogFilterText.Text;
+        if(filterText.Length == 0) return;
+        
+        if(entry.Message.Contains(filterText, StringComparison.Ordinal)) return;
+        if(entry.Source.Contains(filterText, StringComparison.Ordinal)) return;
+
+        e.Accepted = false;
+    }
+
+    private void LogFilterText_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        var textBox = (TextBox)sender;
+        var collection = (CollectionViewSource)textBox.FindResource("LogEntryCollection");
+        collection.View.Refresh();
     }
 }
