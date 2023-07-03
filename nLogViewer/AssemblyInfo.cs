663ef6d4c16c800166a610ef7b72acabcdda7eac
DadonovAleksandr <a.snafu@ya.ru>
2023-07-03 12:28:52 +0300

    Промежуточный коммит

--- nLogViewer/AssemblyInfo.cs
+++ nLogViewer/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Windows;
 
-[assembly: AssemblyVersion("1.1.0702.1239")]
-[assembly: AssemblyFileVersion("1.1.0702.1239")]
+[assembly: AssemblyVersion("1.2.0702.1243")]
+[assembly: AssemblyFileVersion("1.2.0702.1243")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
