5ff77c664b60a63e961c8b81801f6240e3e87b47
DadonovAleksandr <a.snafu@ya.ru>
2023-08-14 23:07:15 +0300

    Пересборка проекта

--- nLogViewer/AssemblyInfo.cs
+++ nLogViewer/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Windows;
 
-[assembly: AssemblyVersion("1.4.0812.1216")]
-[assembly: AssemblyFileVersion("1.4.0812.1216")]
+[assembly: AssemblyVersion("1.6.0812.1223")]
+[assembly: AssemblyFileVersion("1.6.0812.1223")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
