4201ad3375975e79179b8ecb265ddc2ad8d77029
DadonovAleksandr <a.snafu@ya.ru>
2024-07-08 10:29:19 +0300

    Пересборка тестового проекта под NET 8

--- nLogViewer.Tester/AssemblyInfo.cs
+++ nLogViewer.Tester/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Windows;
 
-[assembly: AssemblyVersion("1.1.0705.1520")]
-[assembly: AssemblyFileVersion("1.1.0705.1520")]
+[assembly: AssemblyVersion("1.1.0708.1021")]
+[assembly: AssemblyFileVersion("1.1.0708.1021")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
