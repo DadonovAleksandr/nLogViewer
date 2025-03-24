3b7317d3592784041d24fb0f9ecd0fc5afc105ed
DadonovAleksandr <a.snafu@ya.ru>
2025-03-24 10:25:44 +0300

    Промежуточный коммит

--- nLogViewer/App.xaml.cs
+++ nLogViewer/App.xaml.cs
@@ -3,9 +3,12 @@ using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using NLog;
+using NLog.Extensions.Logging;
 using nLogViewer.Model;
 using nLogViewer.Model.AppSettings.AppConfig;
 using nLogViewer.Services;
@@ -24,7 +27,7 @@ namespace nLogViewer
         private static IHost _host;
         public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs())
             .Build();
-
+        public IConfiguration Configuration { get; }
         protected override async void OnStartup(StartupEventArgs e)
         {
             _log.Debug($"Запуск приложения: {AppConst.Get().AppName} {ProjectVersion.Get()}");
@@ -46,7 +49,14 @@ namespace nLogViewer
 
         public static void ConfigureServices(HostBuilderContext host, IServiceCollection services) => services
             .RegisterServices()
-            .RegisterViewModels();
+            .RegisterViewModels()
+            .AddLogging(builder =>
+            {
+                var logLevel = Configuration.GetValue<string>("Logging:LogLevel:Default");
+                LogManager.Configuration.Variables["logLevel"] = logLevel;
+                builder.ClearProviders();
+                builder.AddNLog();
+            });
 
         public static string CurrentDirectory => IsDesighnMode
             ? Path.GetDirectoryName(GetSourceCodePath())
