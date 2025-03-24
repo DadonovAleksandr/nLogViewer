8b4b08c10676c85f75f1c5b090b459e68d2cc804
DadonovAleksandr <a.snafu@ya.ru>
2025-03-24 11:00:35 +0300

    исправлен паттерн сообщения

--- nLogViewer/Services/LogReader/FileLogReader/FileLogReader.cs
+++ nLogViewer/Services/LogReader/FileLogReader/FileLogReader.cs
@@ -16,7 +16,7 @@ internal class FileLogReader : ILogReader
     private readonly string _path;
     private long _pos;
     private int _lineCount;
-    private static readonly Regex LogEntryPattern = new(@"^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{4})\s*\|\s*(\w+)\s*\|\s*(.*?)\s*\|\s*(\w+)\s*\|\s*(\d+)\s*\|\s*(\d+)?$");
+    private static readonly Regex LogEntryPattern = new(@"^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{4})\s*\|\s*(\w+)\s*\|\s*(.*?)\s*\|\s*([.\w]+)\s*\|\s*(\d+)\s*\|\s*(\d+)?$");
 
     public FileLogReader(string path, IUserDialogService userDialogService)
     {
